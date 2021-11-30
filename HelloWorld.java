e874d55917dd1edc59b7ed169349156354f966fa
zhongliangwu <94953171+cnzoy@users.noreply.github.com>
2021-11-30 21:55:43 +0800

    上传java文件
    第一次使用GitHub上传文件

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		int a=1;
+		System.out.println("Hello World");
+		System.out.println(a);
+		System.out.println("动力节点");
+	}
+	public static void test(String[] args){
+	int b=1;
+	System.out.println(b);
+		
+	}
+}
+
